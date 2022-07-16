e1a37b1631e3b44b2d5b35c49ed036130391dcfd
Gamma <simonjustin321@gmail.com>
2022-07-17 01:25:25 +0530

    new files

---
+++ src/java_July_Batch_10_30/Varthur.java
@@ -0,0 +1,5 @@
+package java_July_Batch_10_30;
+
+public class Varthur {
+
+}
---
+++ src/java_July_Batch_10_30/jvjvgjv.java
@@ -0,0 +1,17 @@
+package java_July_Batch_10_30;
+
+public class jvjvgjv {
+	
+	//Return type
+	//Non return type Void
+	
+	
+	void sum()
+	{
+		System.out.println("10");
+	}
+	
+	
+	
+
+}
